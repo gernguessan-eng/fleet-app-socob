159498595132908c371add1b59e148bfdbc31ed6
gernguessan-eng <gernguessan@gmail.com>
2026-07-26 14:12:57 +0000

    SUPPRIMER dossier geststock

--- stockpro-fcfa/src/lib/session.ts
+++
@@ -1,8 +0,0 @@
-import { cookies } from "next/headers";
-import { SESSION_COOKIE, verifySessionToken } from "@/lib/auth";
-
-export async function getCurrentUser() {
-  const store = await cookies();
-  const token = store.get(SESSION_COOKIE)?.value;
-  return verifySessionToken(token);
-}
